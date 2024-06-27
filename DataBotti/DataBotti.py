6f3c62e6041913359ee3163c0fa2c46c433ac0cb
jartsanull <langinenj1@gmail.com>
2024-06-27 15:03:47 +0300

    Update DataBotti.py

--- DataBotti/DataBotti.py
+++ DataBotti/DataBotti.py
@@ -4,3 +4,18 @@ import requests
 from bs4 import BeautifulSoup
 from sqlalchemy import create_engine
 
+
+def data_fetch():
+    global data
+    data = [{"id": 1, "name": "Toni"}]
+    return data
+
+data = [{"id": 1, "name": "Toni"}]
+
+def data_clean(data):
+    data.pop(0)
+    return data
+
+clean = data_clean(data)
+
+print(clean)
