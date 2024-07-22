d9f2de86e8a31a02a21d76167e4a10ba2afaa3f5
null <151707803+jartsanull@users.noreply.github.com>
2024-07-22 12:22:22 +0300

    Update DataBotti.py

--- DataBotti/DataBotti.py
+++ DataBotti/DataBotti.py
@@ -1,31 +1,3 @@
-import pandas as pd
-import numpy as np
-import requests
-from bs4 import BeautifulSoup
-from sqlalchemy import create_engine
-
-
-def data_fetch():
-    global data
-    data = [{"id": 1, "name": "Toni"}]
-    return data
-
-data = [{"id": 1, "name": "Toni"}, {"id": 2, "name": "Al"}] #< - temporary stuff for testing
-
-def data_clean(data):
-    data.pop(1)
-    return data
-
-clean = data
-
-def data_analyse(clean):
-   #cleaned = ", ".join(clean for item in clean)
-   #print(f"Result: ${cleaned}")
-   result = ", ".join(f'id: {item["id"]}, name: {item["name"]}' for item in clean) #< - this one is temporary
-
-
-def data_result(result):
-    return f"The result is: \n {result}"
 
 
 data_clean(data)
